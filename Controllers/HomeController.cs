e186077d0851bf34e96593878ac7672242784aff
Tommy <lin103084@gmail.com>
2025-05-07 16:07:05 +0800

    ok

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("123456789");
         }
 
         public IActionResult Privacy()
